1151fe84cdbf03bfd6bca1e91444bfcd04f4a1b3
andygup <agup@esri.com>
2013-05-20 18:54:52 -0600

    initial commit

---
+++ src/com/agup/html5geo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.agup.html5geo;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_main);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
+	
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+	}
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+	}
+
+}
